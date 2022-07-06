272824da7281be6978dfa1a23142f73cf0de223d
arthur tutu <canalmopaaba@gmail.com>
2022-07-06 07:02:42 -0300

    incluindo repositorio

---
+++ desafios/desafio1.js
@@ -0,0 +1,12 @@
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+
+interface Funcionario {  
+    code: number,
+   name: string
+};
+
+let jhon: Funcionario = {
+code : 10
+name : 'John'
+};
---
+++ desafios/desafio2.js
@@ -0,0 +1,29 @@
+// Como podemos melhorar o esse código usando TS? 
+interface basedados = {
+    nome: string,
+    idade: number,
+    profissao: String
+} 
+let pessoa1 : basedados = {
+nome:'maria',
+idade:29,
+profissao:'atriz'
+}
+
+let pessoa2: basedados = {
+    nome: 'roberto',
+idade : 19,
+profissao : 'Padeiro'
+}
+
+let pessoa3 : basedados = {
+    nome: 'laura',
+    idade: 32,
+    profissao: 'Atriz'
+}
+
+let pessoa4: basedados = {
+    nome : 'carlos',
+    idade : 19,
+    profissao : 'padeiro'
+}
